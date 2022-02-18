9a46d8fda5eefd5d96d5fb3b6f57b3cc57671f7b
Himanshu-rathee <x20132689@student.ncirl.ie>
2022-02-18 19:21:55 +0000

    Create FirstInstantiableClass.java

---
+++ First_Instance_Class/FirstInstantiableClass.java
@@ -0,0 +1,11 @@
+public class FirstInstantiableClass{
+	// Attributes
+	private String message = "First reusable class";
+
+	// Class methods
+
+	public void displayMyState(){
+		System.out.println(message);
+	}
+
+}
